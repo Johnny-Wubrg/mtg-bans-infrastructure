e529b1cf4b42be389272a4e75f74302eef7f3dfc
quangdaon <me@quangdao.com>
2024-11-22 22:53:32 -0600

    Remove egress

--- main.tf
+++ main.tf
@@ -48,13 +48,6 @@ resource "aws_security_group" "rds" {
     cidr_blocks = var.db_whitelist
   }
 
-  egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "mtg_bans_rds"
   }
