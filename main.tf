c0733bd752f67481ff1a4b3b1079b4fde2f8c8da
quangdaon <me@quangdao.com>
2024-11-24 19:11:45 -0600

    ALB

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 
 data "tfe_outputs" "core" {
   organization = "Quangdao"
-  workspace = "core_aws"
+  workspace    = "core_aws"
 }
 
 module "mtg_bans_rds" {
@@ -30,7 +30,15 @@ module "mtg_bans_rds" {
 
   vpc_id = data.tfe_outputs.core.values.vpc_id
 
-  username     = var.db_username
-  password     = var.db_password
-  whitelist    = var.db_whitelist
+  username  = var.db_username
+  password  = var.db_password
+  whitelist = var.db_whitelist
+}
+
+module "mtg_bans_alb" {
+  source  = "app.terraform.io/Quangdao/alb-listener/aws"
+  version = "0.0.2"
+
+  quinfrastructure = data.tfe_outputs.core.values
+  name             = "mtg-bans"
 }
